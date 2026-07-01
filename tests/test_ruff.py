88a014596f5d80ab1e66b91dd2239db99e2f85ce
DesignThinkerer <34781168+DesignThinkerer@users.noreply.github.com>
2026-07-01 21:31:18 +0000

    chore: auto-format with ruff

--- tests/test_ruff.py
+++ tests/test_ruff.py
@@ -1,8 +1,7 @@
-import os
-import sys # Unused import
 
-def my_messy_function(   ):
-    x=10
-    y   = 20
+
+def my_messy_function():
+    x = 10
+    y = 20
     # This line is intentionally way too long to see if the Ruff formatter will chop it down to adhere to the 79 character limit set in thetoml file
-    return x+y
+    return x + y
