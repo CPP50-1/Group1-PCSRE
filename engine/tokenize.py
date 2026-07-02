d690753bf60ca4c663f23f0b5b8df4e5c2e700a5
Théophile Desmedt <34781168+DesignThinkerer@users.noreply.github.com>
2026-07-02 08:36:43 +0000

    refactor: improve tokenizer function to use set for formatted tokens, refactor if condition

--- engine/tokenize.py
+++ engine/tokenize.py
@@ -1,17 +1,15 @@
 import re
 
 
-def tokenizer(str):
+def tokenizer(str) -> set:
     """
     Tokenise by splitting on spaces and punctuation, lowercasing, stripping short words (≤ 2 chars)
     """
     raw_tokens = re.findall(r"\w+", str)
 
-    formatted_tokens = []
+    formatted_tokens = set()
 
     for token in raw_tokens:
-        if len(token) <= 2:
-            continue
-        formatted_tokens.append(token.lower())
-
+        if len(token) > 2:
+            formatted_tokens.add(token.lower())
     return formatted_tokens
