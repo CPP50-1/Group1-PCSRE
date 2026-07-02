68e1b7a325d9308140caf6dcff451e448286e3bb
Théophile Desmedt <34781168+DesignThinkerer@users.noreply.github.com>
2026-07-02 09:05:05 +0000

    fix: streamline tokenizer to exclude short words directly in the loop

--- engine/tokenize.py
+++ engine/tokenize.py
@@ -1,17 +1,16 @@
 import re
 
+
 def tokenizer(str):
     """
     Tokenise by splitting on spaces and punctuation, lowercasing, stripping short words (≤ 2 chars)
     """
-    raw_tokens = re.findall(r'\w+', str)
+    raw_tokens = re.findall(r"\w+", str)
 
     formatted_tokens = []
 
     for token in raw_tokens:
-        if len(token)<=2:
-            continue
-        print(token.lower())
-        formatted_tokens.append(token.lower())
-    
+        if len(token) > 2:
+            formatted_tokens.append(token.lower())
+
     return formatted_tokens
