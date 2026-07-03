395567015b9cad52f9174231d162fa60664f5039
leroden70 <denis@glewlwyd/com>
2026-07-03 11:47:34 +0200

    fix: pre-commit

--- tests/test_suggest.py
+++ tests/test_suggest.py
@@ -4,10 +4,12 @@ from engine.suggest import levenshteinDistance, suggest
 def test_levenshteinDistance():
     assert levenshteinDistance("abcd", "ebcfg") == 3
 
+
 def test_suggest_existing_tokens_only(capsys):
     suggest("Keyboard Monitor")
     captured = capsys.readouterr()
     assert captured.out == ""
+
 
 def test_suggest_title_non_existing_tokens_only(capsys):
     suggest("Keybard Montor")
