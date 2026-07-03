9ef575051fa8c4d833c17071b22531c783de91ef
leroden70 <denis@glewlwyd/com>
2026-07-03 15:44:41 +0200

    refactor: remake levenshtein function

--- engine/suggest.py
+++ engine/suggest.py
@@ -7,7 +7,7 @@ def levenshteinDistance(s1, s2):
     ltable.append([c for c in range(0, len(s2) + 1)])
     for r in range(1, len(s1) + 1):
         ltable.append([r])
-        ltable.extend([0 for c in range(1, len(s2) + 1)])
+        ltable[r].extend([0 for c in range(1, len(s2) + 1)])
     for i in range(1, len(s1) + 1):
         for j in range(1, len(s2) + 1):
             if s1[i - 1] == s2[j - 1]:
