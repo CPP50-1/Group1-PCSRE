a0f01c020daec1aaf462a9bb581fd1455fee7bb2
leroden70 <denis@glewlwyd/com>
2026-07-03 15:56:43 +0200

    refactor: remake levenshtein function

--- engine/suggest.py
+++ engine/suggest.py
@@ -4,10 +4,23 @@ from engine.tokenize import tokenizer
 
 def levenshteinDistance(s1, s2):
     ltable = list(list())
+    # create a table for the distance calculation :
+    # [
+    #   [0, 1, 2, 3, ... , C],
+    #   [1, 0, 0, 0, ... , 0],
+    #   [2, 0, 0, 0, ... , 0],
+    #   [3, 0, 0, 0, ... , 0],
+    #   ...
+    #   [R, 0, 0, 0, ... , 0]
+    # ]
+    # Where R and C are the lengths of the first and second string
     ltable.append([c for c in range(0, len(s2) + 1)])
     for r in range(1, len(s1) + 1):
         ltable.append([r])
         ltable[r].extend([0 for c in range(1, len(s2) + 1)])
+
+    # Implement levenshtein logic
+
     for i in range(1, len(s1) + 1):
         for j in range(1, len(s2) + 1):
             if s1[i - 1] == s2[j - 1]:
